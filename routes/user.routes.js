ad7b1751132cdcfa238d17268d076facfc115eed
Dafa Armanto <randikadafa07@gmail.com>
2025-02-21 15:36:18 +0700

    Add authorize middleware

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,17 @@
 import { Router } from "express";
 import { getUser, getUsers } from "../controllers/user.controller.js";
+import authorize from "../middlewares/auth.middleware.js";
 
 const userRouter = Router();
 
 userRouter.get("/", getUsers);
-userRouter.get("/:id", getUser);
+
+userRouter.get("/:id", authorize, getUser);
+
 userRouter.post("/", (req, res) => res.send({ title: "CREATE new user" }));
+
 userRouter.put("/:id", (req, res) => res.send({ title: "UPDATE user" }));
+
 userRouter.delete("/:id", (req, res) => res.send({ title: "DELETE a user" }));
 
 export default userRouter;
