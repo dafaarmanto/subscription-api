65996776520e14e425218c6c2f46f93f38b0eab3
Dafa Armanto <randikadafa07@gmail.com>
2025-02-25 13:47:46 +0700

    Check authorize user to see subscription

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,29 +1,36 @@
 import { Router } from "express";
 import authorize from "../middlewares/auth.middleware.js";
-import { createSubscription } from "../controllers/subscription.controller.js";
+import {
+  createSubscription,
+  getUserSubscriptions,
+} from "../controllers/subscription.controller.js";
 
 const subscriptionRouter = Router();
 
 subscriptionRouter.get("/", (req, res) =>
   res.send({ title: "GET all subscriptions" })
 );
+
 subscriptionRouter.get("/:id", (req, res) =>
   res.send({ title: "GET subscription details" })
 );
+
 subscriptionRouter.post("/", authorize, createSubscription);
 
 subscriptionRouter.put("/:id", (req, res) =>
   res.send({ title: "UPDATE subscription" })
 );
+
 subscriptionRouter.delete("/:id", (req, res) =>
   res.send({ title: "DELETE a subscriptions" })
 );
-subscriptionRouter.get("/user/:id", (req, res) =>
-  res.send({ title: "GET all user subscriptions" })
-);
+
+subscriptionRouter.get("/user/:id", authorize, getUserSubscriptions);
+
 subscriptionRouter.put("/:id/cancel", (req, res) =>
   res.send({ title: "CANCEL subscription" })
 );
+
 subscriptionRouter.get("/upcoming-renewals", (req, res) =>
   res.send({ title: "GET upcoming renewals" })
 );
